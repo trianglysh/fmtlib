ea24ae158b3dbb42ffbb2dff44b4b625e12d31d6
trianglysh <trianglxboi@gmail.com>
2024-11-27 18:00:46 +0300

    Add Project Files and Build Script

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+project "fmtlib"
+	language "C++"
+	cppdialect "C++17"
+	kind "StaticLib"
+	
+	targetdir "%{OutputDir.Binaries}/%{prj.name}"
+	objdir    "%{OutputDir.Intermediates}/%{prj.name}"
+	
+	files
+	{
+		"include/**.h",
+		"src/**.cc"
+	}
+	
+	includedirs
+	{
+		"include"
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+	
+	filter "configurations:Development"
+		runtime "Debug"
+		symbols "On"
+	
+	filter "configurations:Preview or Shipping"
+		runtime "Release"
+		optimize "Speed"
+	
